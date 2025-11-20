df0b294add2733ba66681b8b5de2662e8e0b11bb
yashvishwakrma132-pixel <yashvishwakrma132@gmail.com>
2025-11-20 10:49:07 +0530

    Create First.c

---
+++ First.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+      Int main()
+       {
+           Int num;
+                
+           printf("enter a number");
+            scanf("%d",number);
+     
+          If(num % 2 ==0);
+          {
+            printf("%d is even\n);
+            }
+        else {
+                   printf("%d is odd\n);
+        
+        return 0;
+       
+ }
